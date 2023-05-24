bf5e91d573f08d16be728fc325e7af9dcac3e798
Maxime SIMON <maxime.simon@icloud.com>
2023-05-24 16:09:07 +0200

    refactor: extract http handling method

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,25 @@ use worker::*;
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     let router = Router::new();
 
-    // GET /hello/{name}, returning a JSON with its access stats
     router
-        .get_async("/hello/:name", |_, ctx| async move {
-            let kv = ctx.kv("stats")?;
-            if let Some(name) = ctx.param("name") {
-                let stat = match kv.get(name).json::<i64>().await? {
-                    Some(s) => {
-                        let stat = s + 1;
-                        kv.put(name, stat)?.execute().await?;
-                        stat
-                    }
-                    None => {
-                        kv.put(name, 1)?.execute().await?;
-                        1
-                    }
-                };
-                Response::from_json(&json!({ "name": name, "stat": &stat }))
-            } else {
-                Response::error("Bad Request", 400)
-            }
-        })
+        .get_async("/hello/:name", get_user)
         .run(req, env)
         .await
+}
+
+// GET /hello/{name}, returning a JSON with user name and access stat
+async fn get_user(_req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let kv = ctx.kv("stats")?;
+    if let Some(name) = ctx.param("name") {
+        let id = name.to_lowercase();
+        let stat = match kv.get(&id).json::<u64>().await? {
+            Some(s) => s + 1,
+            None => 1,
+        };
+        kv.put(&id, stat)?.execute().await?;
+        return Response::from_json(&json!({ "name": name, "stat": &stat }));
+    }
+
+
+    Response::error("Bad Request", 400)
 }
