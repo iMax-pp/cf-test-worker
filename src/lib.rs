d20db9dace1f5eec99f9b07bfb30f9ca81b093e9
Maxime SIMON <maxime.simon@icloud.com>
2023-05-24 15:03:44 +0200

    fix kv use

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 use serde_json::json;
-use worker::{*, kv::KvStore};
+use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
-    let kv = KvStore::create("stats")?;
-    let router = Router::with_data(kv);
+    let router = Router::new();
 
     // GET /hello/{name}, returning a JSON with its access stats
     router
         .get_async("/hello/:name", |_, ctx| async move {
+            let kv = ctx.kv("stats")?;
             if let Some(name) = ctx.param("name") {
-                let stat = match ctx.data.get(name).text().await? {
+                let stat = match kv.get(name).json::<i64>().await? {
                     Some(s) => {
-                        let stat = s.parse::<i64>().unwrap() + 1;
-                        ctx.data.put(name, stat.to_string())?.execute().await?;
-                        s
+                        let stat = s + 1;
+                        kv.put(name, stat)?.execute().await?;
+                        stat
                     }
                     None => {
-                        ctx.data.put(name, "1")?.execute().await?;
-                        String::from("1")
+                        kv.put(name, 1)?.execute().await?;
+                        1
                     }
                 };
                 Response::from_json(&json!({ "name": name, "stat": &stat }))
